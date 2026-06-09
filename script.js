a7819e779884ee14241b18fd42f687958ca0c31a
imtiaz769 <jiskaniimtiaz769@gmail.com>
2026-06-09 16:59:25 +0500

    this is my second commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let age=prompt("Enter your age")
 
-if(age>=18)
+if(age>=25)
     console.log("You can vote");
 else
     console.log("You can not vote");
     
+let name="Hashim"
+console.log("this is ",name);
